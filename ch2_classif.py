14644ec398ac406eeffd101c522e63a9c2b5c8c9
Finbonkle <jamestball.jb@gmail.com>
2018-12-27 14:25:44 -0800

    first commit

---
+++ ch2_classif.py
@@ -0,0 +1,18 @@
+# James T Ball
+# Following along with the first program in chapter 2
+
+# import packages
+import numpy as np
+from sklearn import preprocessing
+
+# define sample data
+input_data = np.array([[ 5.1, -2.9,  3.3],
+                       [-1.2,  7.8, -6.1],
+                       [ 3.9,  0.4,  2.1],
+                       [ 7.3, -9.9, -4.5]])
+
+# Binarize data
+data_binarized = preprocessing.Binarizer(threshold=2.1).transform(input_data)
+print("\nBinarized data:\n", data_binarized)
+
+# Print mean and standard deviation
